ec2120bf98a65d7c03e757eff3dd37f85d9d4dd8
lewiskrishnamurti <lkri3129@uni.sydney.edu.au>
2021-09-26 04:21:43 +1000

    Add link to source

--- static/visualisation.js
+++ static/visualisation.js
@@ -313,7 +313,6 @@ function onPointerMove(event) {
 
 // Used for clicking on faces and vertices
 function onClick(event) {
-    event.preventDefault();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
